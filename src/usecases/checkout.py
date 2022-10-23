0b34b90868f2aed8233499b2c225b8236ddbd2b3
Rafael V <rafaelv.dev@gmail.com>
2022-10-23 22:02:57 +0200

    Adding basic checkout logic and api (without discounts)

--- src/usecases/checkout.py
+++ src/usecases/checkout.py
@@ -1,11 +1,35 @@
+import json
+
+from moneyed import Money
+from moneyed.l10n import format_money
+
 class CheckoutInteractor:
 
     def __init__(self, price_rules: str):
-        self.price_rules = price_rules
+        self.products = {}
+        self.items = []
+        self.load_price_rules(price_rules)
+
+    def load_price_rules(self, price_rules_json: str) -> None:
+        price_rules = json.loads(price_rules_json)
+        for product in price_rules:
+            amount, currency = product["price"].split(" ")
+            self.products[product["code"]] = {
+                "name": product["name"],
+                "price": Money(amount, currency),
+            }
 
     def scan(self, product_code: str) -> None:
-        return self.price_rules
+        if product_code in self.products.keys():
+            self.items.append(product_code)
+        else:
+            raise ValueError("Product code not found!")
 
     @property
     def total(self) -> str:
-        return "5.00€"
+        result = Money("0.00", "EUR")
+        for item in self.items:
+            result += self.products[item]["price"]
+
+        formatted_result = format_money(result, "0.00¤", locale="en_US")
+        return formatted_result
--- tests/usecases/test_checkout_interactor.py
+++ tests/usecases/test_checkout_interactor.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from src.usecases.checkout import CheckoutInteractor
@@ -6,9 +7,37 @@ from src.usecases.checkout import CheckoutInteractor
 class TestCheckoutInteractor(unittest.TestCase):
 
     def setUp(self):
-        self.price_rules = "{}"
-        self.checkout = CheckoutInteractor(self.price_rules)
+        # Basic Products and Discount test fixture
+        price_rules = [
+                {
+                    "code": "VOUCHER",
+                    "name": "Voucher",
+                    "price": "5.00 EUR"
+                },
+                {
+                    "code": "TSHIRT",
+                    "name": "T-shirt",
+                    "price": "20.00 EUR"
+                },
+                {
+                    "code": "MUG",
+                    "name": "Coffee mug",
+                    "price": "7.50 EUR"
+                }
+        ]
+        price_rules_json = json.dumps(price_rules)
+        self.checkout = CheckoutInteractor(price_rules_json)
 
-    def test_simple_case(self):
+    def test_raise_error_product_code_not_found(self):
+        with self.assertRaises(ValueError):
+            self.checkout.scan("FOO")
+
+    def test_empty_checkout(self):
+        self.assertEqual(self.checkout.total, "0.00€")
+
+    def test_simple_checkout(self):
         self.checkout.scan("VOUCHER")
-        self.assertEqual(self.checkout.total, "5.00€")
+        self.checkout.scan("TSHIRT")
+        self.checkout.scan("MUG")
+        self.assertEqual(self.checkout.total, "32.50€")
+
