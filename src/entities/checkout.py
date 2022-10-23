21c4ee8781ef5e398173106fa3d1b0004d8e4e48
Rafael V <rafaelv.dev@gmail.com>
2022-10-23 22:46:05 +0200

    Add unit price as Money instance to product entity.

--- src/entities/checkout.py
+++ src/entities/checkout.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
 from typing import Union
 
+from moneyed import Money
+
 @dataclass
 class Product:
     """Class for keeping track of a Product"""
-    code: str = None
-    name: str = None
-    price_models: str = None
+    code: str
+    name: str
+    price: str
+
+    @property
+    def unit_price(self) -> Money:
+        return Money(self.price["amount"], self.price["currency_code"])
--- tests/entities/test_checkout_entities.py
+++ tests/entities/test_checkout_entities.py
@@ -1,5 +1,7 @@
 import unittest
 
+from moneyed import Money
+
 from src.entities.checkout import Product
 
 
@@ -8,11 +10,20 @@ class TestProduct(unittest.TestCase):
         product = {
             "code": "VOUCHER",
             "name": "Voucher",
-            "price_models": "5.00 EUR"
+            "price": {
+                "amount": "5.00",
+                "currency_code": "EUR"
+            }
         }
         self.product = Product(**product)
 
     def test_product_properties(self):
         self.assertEqual(str(self.product.code), "VOUCHER")
         self.assertEqual(self.product.name, "Voucher")
-        self.assertEqual(self.product.price_models, "5.00 EUR")
+        self.assertEqual(
+            self.product.price, {"amount": "5.00","currency_code": "EUR"}
+        )
+        unit_price = self.product.unit_price
+        self.assertIsInstance(unit_price, Money)
+        self.assertEqual(unit_price.get_amount_in_sub_unit(), 500)
+        self.assertEqual(unit_price.currency.code, "EUR")
